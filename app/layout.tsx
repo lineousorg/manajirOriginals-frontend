6f4d6f957412efa07f3018ad1c333f1b8168cb5f
rezalineous-crypto <rezalineous@gamil.com>
2026-05-05 11:50:33 +0600

    chore(config): replace Meta Pixel with Google Tag Manager
    Remove Meta Pixel script and noscript fallback from the root layout and implement Google Tag Manager (GTM) instead.

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,24 +29,20 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Meta Pixel Code */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1334581931761552');
-              fbq('track', 'PageView');
-            `,
-          }}
-        />
-        <noscript>
+         {/* Google Tag Manager */}
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-MLG8FP2F');
+        `,
+      }}
+    />
+    {/* End Google Tag Manager */}
+        {/* <noscript>
           <img
             height="1"
             width="1"
@@ -54,10 +50,23 @@ export default function RootLayout({
             src="https://www.facebook.com/tr?id=1334581931761552&ev=PageView&noscript=1"
             alt=""
           />
-        </noscript>
+        </noscript> */}
         {/* End Meta Pixel Code */}
+
+    {/* End Google Tag Manager (noscript) */}
       </head>
       <body className={`${inter.variable} ${cormorant.variable}`}>
+
+        
+            {/* Google Tag Manager (noscript) */}
+    <noscript>
+      <iframe
+        src="https://www.googletagmanager.com/ns.html?id=GTM-MLG8FP2F"
+        height="0"
+        width="0"
+        style={{ display: "none", visibility: "hidden" }}
+      />
+    </noscript>
         <div className="glass-overlay text-center">
           <Providers>
             <Toaster />
